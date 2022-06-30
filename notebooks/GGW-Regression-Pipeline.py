e910f268cb55590dffbe5cd58359c8a345041fe2
ggwiebe <ggwiebe>
2022-06-30 13:38:51 +0000

    Descriptive notebook rename & train options

--- notebooks/GGW-Regression-Pipeline.py
+++ notebooks/GGW-Regression-Pipeline.py
@@ -1,9 +1,8 @@
 # Databricks notebook source
-
 # MAGIC %md
 # MAGIC # MLflow Regression Pipeline Databricks Notebook
 # MAGIC This notebook runs the MLflow Regression Pipeline on Databricks and inspects its results.
-# MAGIC
+# MAGIC 
 # MAGIC For more information about the MLflow Regression Pipeline, including usage examples,
 # MAGIC see the [Regression Pipeline overview documentation](https://mlflow.org/docs/latest/pipelines.html#regression-pipeline)
 # MAGIC and the [Regression Pipeline API documentation](https://mlflow.org/docs/latest/python_api/mlflow.pipelines.html#module-mlflow.pipelines.regression.v1.pipeline).
--- steps/train.py
+++ steps/train.py
@@ -5,13 +5,27 @@ This module defines the following routines used by the 'train' step of the regre
   during training to produce a model pipeline.
 """
 
+def tree_estimator():
+  from sklearn.tree import DecisionTreeRegressor
+  
+  return DecisionTreeRegressor(random_state=42, criterion='mse', max_depth=5)
 
-def estimator_fn():
+
+def sgd_estimator():
     """
     Returns an *unfitted* estimator that defines ``fit()`` and ``predict()`` methods.
     The estimator's input and output signatures should be compatible with scikit-learn
     estimators.
     """
     from sklearn.linear_model import SGDRegressor
-
+    
     return SGDRegressor(random_state=42)
+
+"""
+estimator_fn() is the entrypoint for train.py
+
+By defining two estimators, we can demo starting with sgd_estimator, then move to the decision tree which gives better performance for this case
+"""
+def estimator_fn():
+#   return sgd_estimator()
+  return tree_estimator()
